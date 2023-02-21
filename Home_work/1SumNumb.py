41ec6fe88a3d8b04e9cc60a2809f91c98fc5b55e
“KurtsKurts” <Alex23.89@mail.ru>
2023-02-21 10:35:18 +0700

    Paper bird

--- Home_work/1SumNumb.py
+++ Home_work/1SumNumb.py
@@ -3,7 +3,7 @@
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0)
 
-Tree_numb=int(input('Введите трехзначное число : '))
+Tree_numb = int(input('Введите трехзначное число : '))
 
-sum = int(Tree_numb%10 + Tree_numb%100/10 + Tree_numb%1000/100)
+sum = int(Tree_numb % 10 + Tree_numb % 100/10 + Tree_numb % 1000/100)
 print(f'Сумма цифр трехзначного числа = {sum}')
